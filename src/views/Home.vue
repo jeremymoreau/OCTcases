0903cc6ce34527d3f7887e5118e04e0573a494ff
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2021-08-22 12:40:32 -0600

    Move non-user image assets out of media dir

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,10 +10,10 @@
           <ion-card-content>
             <picture>
               <source
-                srcset="/content/media/OCTcases-logo-dark.png"
+                srcset="/assets/img/OCTcases-logo-dark.png"
                 media="(prefers-color-scheme: dark)"
               />
-              <img src="/content/media/OCTcases-logo-light.png" />
+              <img src="/assets/img/OCTcases-logo-light.png" />
             </picture>
             <!-- <img src="/content/media/OCTcases-logo.png"> -->
             <!-- <img src="/content/media/home-sample-image.png"> -->
@@ -35,7 +35,7 @@
 
           <ion-card-content>
             <h1>Real Patients</h1>
-            <img src="/content/media/home-features1.png" />
+            <img src="/assets/img/home-features1.png" />
             <ion-text
               >All cases presented on OCTcases are from our own clinical
               practice, and thus are reflective of real patient
@@ -43,7 +43,7 @@
             >
 
             <h1 class="feature-title">Interactive</h1>
-            <img src="/content/media/home-features2.png" />
+            <img src="/assets/img/home-features2.png" />
             <ion-text
               >Our diagrams are immersive and built for our users. Our images
               are labelled with accompanying descriptions and clinical
@@ -51,7 +51,7 @@
             >
 
             <h1 class="feature-title">Detailed Answers</h1>
-            <img src="/content/media/home-features3.png" />
+            <img src="/assets/img/home-features3.png" />
             <ion-text
               >All questions are carefully crafted and high yield. Each question
               comes with labelled diagrams and detailed explanations.</ion-text
@@ -71,19 +71,19 @@
               <img
                 class="logos"
                 id="logo-bayer"
-                src="/content/media/home-logo-bayer.png"
+                src="/assets/img/home-logo-bayer.png"
             /></a>
             <a href="http://www.eyeguru.org/" target="_blank">
-              <img class="logos" src="/content/media/home-logo-eyeguru.png"
+              <img class="logos" src="/assets/img/home-logo-eyeguru.png"
             /></a>
             <a href="https://ophthalmology.utoronto.ca/" target="_blank">
-              <img class="logos" src="/content/media/home-logo-uoft.png"
+              <img class="logos" src="/assets/img/home-logo-uoft.png"
             /></a>
             <a
               href="https://www.cosprc.ca/optical-coherence-tomography-oct-learning-website/"
               target="_blank"
             >
-              <img class="logos" src="/content/media/home-logo-prc.png"
+              <img class="logos" src="/assets/img/home-logo-prc.png"
             /></a>
           </ion-card-content>
         </ion-card>

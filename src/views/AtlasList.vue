29c981ee45a5a2d51b0808e589fd04fc33413301
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2021-09-05 15:19:15 -0600

    Add atlas list UI layout

--- src/views/AtlasList.vue
+++ src/views/AtlasList.vue
@@ -1,19 +1,53 @@
 <template>
   <ion-page>
-      <Header></Header>
+    <Header></Header>
 
-      <ion-content overflow-scroll="true">
-        <ion-list id="array-rendering" lines="full">
+    <ion-content overflow-scroll="true">
+      <IonSearchbar></IonSearchbar>
+      <ion-card class="atlas-card" @click="openTest()">
+        <img src="/content/media/retina2-q1.png" />
+        <ion-card-header class="atlas-card-header">
+          <!-- <ion-card-subtitle>test</ion-card-subtitle> -->
+          <ion-card-title class="atlas-card-title">
+            Acute Annular Outer Retinopathy
+          </ion-card-title>
+        </ion-card-header>
 
-        </ion-list>
-      </ion-content>
+        <ion-card-content>
+          <ion-text class="md-text">
+            AAOR is characterised by thinning or atrophy of the outer
+            retina/ONL, including disruption of the EZ within the annular area
+            of RPE destruction.
+          </ion-text>
+        </ion-card-content>
+      </ion-card>
+      <ion-card class="atlas-card" @click="openTest()">
+        <img src="/content/media/retina2-q1.png" />
+        <ion-card-header class="atlas-card-header">
+          <!-- <ion-card-subtitle>test</ion-card-subtitle> -->
+          <ion-card-title class="atlas-card-title">
+            Acute Posterior Multifocal Placoid Pigment Epitheliopathy
+          </ion-card-title>
+        </ion-card-header>
 
-      <Footer></Footer>
+        <ion-card-content>
+          <ion-text class="md-text">
+            AAOR is characterised by thinning or atrophy of the outer
+            retina/ONL, including disruption of the EZ within the annular area
+            of RPE destruction.
+          </ion-text>
+        </ion-card-content>
+      </ion-card>
+
+      <ion-list id="array-rendering" lines="full"> </ion-list>
+    </ion-content>
+
+    <Footer></Footer>
   </ion-page>
 </template>
 
 <script lang='js'>
-import { IonPage, IonList } from "@ionic/vue";
+import { IonPage, IonList, IonSearchbar, IonCard } from "@ionic/vue";
 import { getJSON } from "./helpers";
 
 import { defineComponent } from "vue";
@@ -25,6 +59,8 @@ export default defineComponent({
   components: {
     IonPage,
     IonList,
+    IonSearchbar,
+    IonCard,
     Footer,
     Header,
   },
@@ -40,6 +76,9 @@ export default defineComponent({
         params: { slug: slug },
       });
     },
+    openTest() {
+      console.log('test')
+    }
   },
 });
 </script>

5c10a5ad034ae7d3af524bef3c813e01f85f9766
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2021-09-06 15:39:01 -0600

    Add index generation for atlas articles

--- scripts/generate_index.js
+++ scripts/generate_index.js
@@ -22,7 +22,6 @@ module.exports = function generateIndex(ctx) {
             caseIndex.push(currentCase)
         }
     })
-
     const indexFilePath = './public/assets/index/case_index.json'
     fs.writeFileSync(indexFilePath, JSON.stringify(caseIndex));
 
@@ -46,7 +45,26 @@ module.exports = function generateIndex(ctx) {
     articleIndex.sort(function(a, b){
         return a.sortOrder - b.sortOrder;
     });
-
     const articleIndexFilePath = './public/assets/index/article_index.json'
     fs.writeFileSync(articleIndexFilePath, JSON.stringify(articleIndex));
+
+    // Generate and Index JSON file of all the atlas in /public/content/atlas
+    const atlasIndex = []
+    fs.readdirSync("./public/content/atlas").forEach((file) => {
+        if (file.endsWith('json')) {
+            const filePath = path.join('./public/content/atlas/', file)
+            const jsonData = JSON.parse(fs.readFileSync(filePath))
+            
+            const currentAtlas = {
+                "slug": file.slice(0, -5), // remove .json from string
+                "title": jsonData.title,
+                "description": jsonData.description,
+                "mainImage": jsonData.mainImage
+            }
+            atlasIndex.push(currentAtlas)
+        }
+    })
+
+    const atlasIndexFilePath = './public/assets/index/atlas_index.json'
+    fs.writeFileSync(atlasIndexFilePath, JSON.stringify(atlasIndex));
 }
